35eb746d800623071ac27ee579cc07d7cca752a1
youyo <1003ni2@gmail.com>
2017-02-05 17:52:01 +0900

    bugfix

--- config/software/itamae.rb
+++ config/software/itamae.rb
@@ -1,12 +1,11 @@
 #
 
 name "itamae"
-version "1.9.10"
 
 dependency "ruby"
 dependency "rubygems"
 
 build do
   gem "install thor -v '0.19.1' --no-rdoc --no-ri"
-	gem "install #{name} -v '#{version}' -n #{install_dir}/bin --no-rdoc --no-ri"
+  gem "install #{name} -v '1.9.10' -n #{install_dir}/bin --no-rdoc --no-ri"
 end
