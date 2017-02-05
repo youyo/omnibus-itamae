4d24c487b79c29d56306055e64c0b05ce89dfe54
youyo <1003ni2@gmail.com>
2017-02-05 17:50:17 +0900

    bugfix. https://github.com/itamae-kitchen/itamae/issues/234

--- config/software/itamae.rb
+++ config/software/itamae.rb
@@ -1,10 +1,12 @@
 #
 
 name "itamae"
+version "1.9.10"
 
 dependency "ruby"
 dependency "rubygems"
 
 build do
-	gem "install #{name} -n #{install_dir}/bin --no-rdoc --no-ri"
+  gem "install thor -v '0.19.1' --no-rdoc --no-ri"
+	gem "install #{name} -v '#{version}' -n #{install_dir}/bin --no-rdoc --no-ri"
 end
