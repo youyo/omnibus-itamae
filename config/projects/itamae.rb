cab89fd475d6fb1b2c59b0e30b8f3482702ffb98
youyo <1003ni2@gmail.com>
2017-02-05 18:44:17 +0900

    Do not add timestamp.

--- config/projects/itamae.rb
+++ config/projects/itamae.rb
@@ -8,8 +8,10 @@ install_dir "/opt/#{name}"
 
 build_version Omnibus::BuildVersion.semver
 build_iteration 1
+append_timestamp false
 
 dependency "preparation"
+dependency "rake"
 dependency "itamae"
 dependency "serverspec"
 dependency "awspec"
