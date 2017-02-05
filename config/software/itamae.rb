2109655510001e959b9aac5ce5a0b07f24631847
youyo <1003ni2@gmail.com>
2017-02-05 16:52:07 +0900

    fix

--- config/software/itamae.rb
+++ config/software/itamae.rb
@@ -1,11 +1,10 @@
 #
 
 name "itamae"
-version "1.9.10"
 
 dependency "ruby"
 dependency "rubygems"
 
 build do
-	gem "install #{name} -v #{version} -n #{install_dir}/bin --no-rdoc --no-ri"
+	gem "install #{name} -n #{install_dir}/bin --no-rdoc --no-ri"
 end
